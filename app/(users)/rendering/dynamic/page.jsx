c820aaa4f434f5aac9254aa262fa67ae3b4ae716
Deepak <deepakshakya3540@gmail.comm>
2025-07-03 06:20:02 +0530

    Static and Dynamic Rendering

---
+++ app/(users)/rendering/dynamic/page.jsx
@@ -0,0 +1,28 @@
+import { db } from "@/config/db.jsx";
+
+export const dynamic = 'force-dynamic';
+
+const DynamicPage = async () => {
+  const [patients] = await db.execute("Select * from patients");
+  console.log("Dynamic patients");
+
+  return (
+    <>
+    <div className="flex justify-center align-middle">
+
+      <ul className="bg-sky-300 border-2 border-red border-dashed">
+        {patients.map((data) => {
+          return (
+            <>
+            <li key={data.id}>{data.name} , Age {data.age} , {data.gender} , Diseases = {data.disease}</li>
+
+            </>
+          )
+        })}
+      </ul>
+    </div>
+    </>
+  );
+};
+
+export default DynamicPage;
