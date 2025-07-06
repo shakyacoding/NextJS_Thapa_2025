2ab651d0f2bc763c7a60f9f4a8a7838e04c90db6
Deepak <deepakshakya3540@gmail.comm>
2025-07-06 16:52:43 +0530

    cache

--- app/(users)/rendering/dynamic/page.jsx
+++ app/(users)/rendering/dynamic/page.jsx
@@ -1,28 +1,48 @@
 import { db } from "@/config/db.jsx";
+import {cache} from 'react';
+
 // test 
 export const dynamic = 'force-dynamic';
 
 const DynamicPage = async () => {
-  const [patients] = await db.execute("Select * from patients");
-  console.log("Dynamic patients");
+
+  const patients = await getAllPatients()
+
+
 
   return (
     <>
-    <div className="flex justify-center align-middle">
-
-      <ul className="bg-sky-300 border-2 border-red border-dashed">
-        {patients.map((data) => {
-          return (
-            <>
-            <li key={data.id}>{data.name} , Age {data.age} , {data.gender} , Diseases = {data.disease}</li>
-
-            </>
-          )
-        })}
-      </ul>
-    </div>
+      <p>Total patients: {patients.length}</p>
+      <PatientLists patients={patients} />
     </>
   );
 };
 
 export default DynamicPage;
+
+const PatientLists = async () => {
+  const patients = await getAllPatients()
+  return (
+    <>
+      <div className="flex justify-center align-middle">
+
+        <ul className="bg-sky-300 border-2 border-red border-dashed">
+          {patients.map((data) => {
+            return (
+              <>
+                <li key={data.id}>{data.name} , Age {data.age} , {data.gender} , Diseases = {data.disease}</li>
+
+              </>
+            )
+          })}
+        </ul>
+      </div>
+    </>
+  )
+}
+
+const getAllPatients =  cache( async () => {
+  const [patients] = await db.execute("Select * from patients");
+  console.log('fetching patients')
+  return patients;
+})
