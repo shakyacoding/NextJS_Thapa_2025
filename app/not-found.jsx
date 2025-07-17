802debc2023de7c1139e2f6307d3630ea8d1556c
Deepak <deepakshakya3540@gmail.comm>
2025-07-17 16:58:15 +0530

    Go back button

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,21 +1,39 @@
+"use client"
 import Link from "next/link";
 
+import { useRouter } from "next/navigation";
+
 export default function NotFoundPage() {
+    const router = useRouter()
     return (
         <>
             <div className="flex justify-center align-middle mt-45">
                 <div>
 
-                    <h1>404 </h1>
-                    <br />
-                    <h2>Page Not Found</h2>
-                    <br />
-                    <button className="bg-black text-2xl text-white border-2 border-black p-2">
-                        <Link href="/">
+                    <div>
 
-                            <span>Go Home page</span>
-                        </Link>
-                    </button>
+                        <h1>404 </h1>
+                        <br />
+                        <h2>Page Not Found</h2>
+                        <br />
+                        <button className="bg-amber-600 text-2xl text-white border-2 border-black p-2">
+                            <Link href="/">
+
+                                <span>Go Home page</span>
+                            </Link>
+                        </button>
+
+                    </div>
+
+                    <div className="mt-10">
+
+                        <button
+                            onClick={() => router.back()}
+                            className="bg-orange-600 text-2xl text-white border-2 border-black p-2">
+
+                            <span>Go Back</span>
+                        </button>
+                    </div>
                 </div>
             </div>
         </>
